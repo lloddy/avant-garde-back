303f8db3af0fba57f04b5dd9c28f5001703e2374
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-10-29 13:03:25 -0400

    WIP changes to Schema to allow more images

--- models/artists.js
+++ models/artists.js
@@ -7,7 +7,47 @@ const artistSchema = new Schema ({
     death: String,
     image: String,
     bio: String,
-    era: String
+    era: String,
+    image1: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image2: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image3: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image4: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image5: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image6: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },        
+    image7: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image8: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image9: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
+    image10: { 
+        type: String,
+        default: "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg",
+    },
 })
 
 module.exports = mongoose.model('Artists', artistSchema)
